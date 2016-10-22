dd886193eb92547841a84c57fde3a9055f4632c7
Kaushal Shah <KaushalShah1307@gmail.com>
2016-10-21 23:15:32 -0400

    edits to config file

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ export default {
   },
   bucket: {
     slug: 'uniblog-using-reactjs',
-    media_url: 'https://api.cosmicjs.com/v1/uniblog-using-reactjs/'
+    media_url: 'https://cosmicjs.com/uploads'
   }
 }
